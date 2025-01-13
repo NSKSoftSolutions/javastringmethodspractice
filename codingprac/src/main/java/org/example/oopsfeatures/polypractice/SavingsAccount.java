5659da80ecd6d482dc8f648404bb1870be6bc8e6
nsajjadkhannavab <sajjadkhannavab@gmail.com>
2025-01-13 12:56:26 +0530

    Added new classes

---
+++ codingprac/src/main/java/org/example/oopsfeatures/polypractice/SavingsAccount.java
@@ -0,0 +1,10 @@
+package org.example.oopsfeatures.polypractice;
+
+public class SavingsAccount extends AccountServices {
+
+    @Override
+    public double getIntresetRate() {
+        return 7.5;
+    }
+
+}
