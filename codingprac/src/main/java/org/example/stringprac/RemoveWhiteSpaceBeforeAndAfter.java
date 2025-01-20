11f8866462e92c5a3d205d24d32c6707fdf96738
nsajjadkhannavab <sajjadkhannavab@gmail.com>
2025-01-20 22:37:55 +0530

    Added new RemoveWhiteSpaceBeforeAndAfter and ClientApp.java

---
+++ codingprac/src/main/java/org/example/stringprac/RemoveWhiteSpaceBeforeAndAfter.java
@@ -0,0 +1,13 @@
+package org.example.stringprac;
+
+public class RemoveWhiteSpaceBeforeAndAfter {
+
+    public String trimWhiteSpace(String name) {
+        System.out.println(name);
+        String resultantString = name.trim();
+        System.out.println(resultantString);
+        return resultantString;
+    }
+
+
+}
---
+++ codingprac/src/main/java/org/example/stringprac/clientapps/ClientApp.java
@@ -0,0 +1,15 @@
+package org.example.stringprac.clientapps;
+
+import org.example.stringprac.RemoveWhiteSpaceBeforeAndAfter;
+
+public class ClientApp {
+
+    public static void main(String[] args) {
+
+        RemoveWhiteSpaceBeforeAndAfter whiteSpaceBeforeAndAfter = new RemoveWhiteSpaceBeforeAndAfter();
+        whiteSpaceBeforeAndAfter.trimWhiteSpace(" Sajjad Ali khan ");
+
+
+    }
+
+}
